6f6d0025be606adf8cf12d8a583454b6a834d2d9
Daniel Sharples <danielsharples83@gmail.com>
2023-02-21 15:43:27 +0000

    completed game

--- javascript.js
+++ javascript.js
@@ -38,7 +38,50 @@ function playRound(playerSelection , computerSelection){
     }
 }
 
+function getPlayerChoice(){
+    let validatedInput = false;
+    while (validatedInput == false){
+        const choice = prompt("Rock, Paper or Scissors?");
+        if(choice === null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
 
-const playerSelection ="rock"
-const computerSelection= getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+}
+
+
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+console.log("Hello!")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+        console.log("_______");
+        if(checkWinner(playerSelection, computerSelection) === "Player"){
+            scorePlayer++;
+        }
+        else if(checkWinner(playerSelection, computerSelection) === "Computer"){
+            scoreComputer++;
+        }
+     }
+
+     console.log("Game Over")
+     if(scorePlayer > scoreComputer){
+        console.log("You Win!");
+     }
+     else if(scorePlayer < scoreComputer){
+        console.log("Computer Wins!");
+     }
+     else{
+        console.log("Its a Draw!");
+     }
+}
+
+game()
